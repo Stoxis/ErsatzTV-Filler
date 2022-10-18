100b57120b4ab1891f43a0454867401578a1c221
liam8888999 <robsonliam2@gmail.com>
2022-10-18 20:46:30 +1100

    Update epg.sh

--- helper-scripts/epg.sh
+++ helper-scripts/epg.sh
@@ -93,7 +93,7 @@ fi
 
 
 
-ffmpeg -y -f lavfi -i color=$offlinebackground1:$videoresolution -stream_loop -1 -i $offlineaudio -shortest -vf "drawtext=textfile='$workdir/upnext$xmltvloop.txt': fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf: x=(w-text_w)/3:y=(h-text_h)/2: fontcolor=$offlinetextcolour1: fontsize=W/40:"  -pix_fmt yuv420p -c:a copy -t 00:00:05.000 $output/epg/$xmltvloop.mp4
+ffmpeg -y -f lavfi -i color=$$offlinebackgroundcolour:$videoresolution:5 -stream_loop -1 -i $offlineaudio -shortest -vf "drawtext=textfile='$workdir/upnext$xmltvloop.txt': fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf: x=(w-text_w)/3:y=(h-text_h)/2: fontcolor=$offlinetextcolour1: fontsize=W/40:"  -pix_fmt yuv420p -c:a copy -t 00:00:05.000 $output/epg/$xmltvloop.mp4
 touch $output/channel-resuming/$xmltvloop.mp4
 awk 'NR>1' $workdir/xmlfiles4.txt > $workdir/xmllll.txt && mv $workdir/xmllll.txt $workdir/xmlfiles4.txt
 
