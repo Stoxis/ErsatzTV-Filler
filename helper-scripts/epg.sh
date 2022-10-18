382e6c8fbe3d1234cfa300c359a082d9816ae8f1
liam8888999 <robsonliam2@gmail.com>
2022-10-18 18:19:58 +1100

    Update epg.sh

--- helper-scripts/epg.sh
+++ helper-scripts/epg.sh
@@ -52,9 +52,8 @@ offlineaudio=$(head -n $randomNumberoffline $workdir/music.txt | tail -n 1)
 
 # get and read xmltv data
 tv_to_text --output $workdir/tempxml$xmltvloop.xml $workdir/xmltv/$xmltvloop.xml
-awk '/news/{p=1}p' $workdir/tempxml$xmltvloop.xml > $workdir/xmltemp$xmltvloop.txt
-awk '!/news/' $workdir/xmltemp$xmltvloop.txt > $workdir/xmltemp2$xmltvloop.xml
-head -1 $workdir/xmltemp2$xmltvloop.xml > $workdir/xmltemp3$xmltvloop.txt
+tail -n +3 "$workdir/tempxml$xmltvloop.xml" > "$workdir/tempxml$xmltvloop.xml.tmp" && mv "$workdir/tempxml$xmltvloop.xml.tmp" "$workdir/tempxml$xmltvloop.xml"
+head -1 $workdir/tempxml2$xmltvloop.xml > $workdir/xmltemp3$xmltvloop.txt
 cut -d "-" -f 1 $workdir/xmltemp3$xmltvloop.txt > $workdir/xmltemp4$xmltvloop.txt
 #news backgound
 #background colour randomiser
